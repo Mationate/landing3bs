df79ff6101e3aa28e75e980ffcb438b47a714e96
Matias Onate <mationate.0@gmail.com>
2024-02-07 13:18:01 -0300

    landing

---
+++ src/components/particles/DataLists.tsx
@@ -0,0 +1,225 @@
+export const NavLinks = [
+    {
+        name: "Destination",
+        url: "/"
+    },
+    {
+        name: "Hotels",
+        url: "/"
+    },
+    {
+        name: "Flights",
+        url: "/"
+    },
+    {
+        name: "Bookings",
+        url: "/"
+    }
+
+];
+
+export const NavButtons = [
+
+    {
+        name: "Entrar",
+        url: "https://www.3bslab.com"
+    }
+
+];
+
+
+export const HeroTexts = {
+    firstText: "CENTRO DE EXPERIENCIA Y APRENDIZAJE",
+    secondText: "Domina el arte de la barbería con 3BSLAB",
+    thirdText: "Bienvenido a 3BSLAB, donde el dominio de la barbería se convierte en un viaje transformador. Sumérgete en el arte de la barbería, explora nuestros variados cursos y desbloquea el potencial para redefinir tu carrera. Nuestra plataforma está diseñada para barberos, emprendedores aspirantes en la industria del cuidado personal y entusiastas del estilismo. Únete a nosotros para dar forma al futuro de la educación en barbería, porque tu viaje comienza aquí.",
+    firstButton: "Descubre más",
+    secondButton: 'Ver video'
+}
+
+
+export const ServiceTexts = {
+    firstText: "CATEGORY",
+    secondText: "We Offer Best Services",
+    cards: [
+        {
+            firstText: "Plataforma en linea",
+            secondText: "Descubre la comodidad de aprender en línea con 3BSLAB. Nuestra plataforma virtual está diseñada para brindarte acceso fácil a cursos de alta claidad." 
+        },
+        {
+            firstText: "Módulos completos",
+            secondText: "Sumérgete en un entorno de aprendizaje cuidadosamente estructurado. En 3BSLAB, cada curso está planificado para ofrecerte la mejor experiencia posible"
+        },
+        {
+            firstText: "Próximamente: Eventos locales",
+            secondText: "Únete a nuestra comunidad activa de estudiantes y profesionales de la barbería. Participa en eventos locales, conéctate con otros apasionados de la industria y amplía tu red de contactos"
+        },
+
+    ]
+}
+
+export const TopDestinationTexts = {
+    firstText: "Explora",
+    secondText: "Algunos de nuestros cursos",
+    cards: [
+        {
+            country: "Rome, Italy",
+            duration: "10 horas"
+        },
+        {
+            country: "London, UK",
+            duration: "25 horas"
+        },
+        {
+            country: "Paris, France",
+            duration: "8 horas"
+        },
+        {
+            country: "New York, USA",
+            duration: "8 horas"
+        },
+        {
+            country: "Warsaw, Poland",
+            duration: "14 horas"
+        },
+        {
+            country: "Dubai, UAE",
+            duration: "40 horas"
+        },
+    ]
+}
+
+export const BookingStepsTexts = {
+    firstText: "",
+    secondText: "Conoce las características de nuestros cursos",
+    listOfSteps: [
+        {
+            text: "Aprendizaje modulado."
+        },
+        {
+            text: "Medios de pago."
+        },
+        {
+            text: "Curso permanente."
+        }
+    ],
+    cardOne:
+    {
+        name: "Visagismo",
+        date: "",
+        people: "24 horas"
+    },
+    cardTwo: {
+        status: "Progreso",
+        destination: "Visagismo",
+        completion: "40% Completado"
+    }
+
+}
+
+export const TestimonialTexts = {
+    firstText: "TESTIMONIALS",
+    secondText: "What people say about Us.",
+    feedBacks: [
+        {
+            text: "You did a fabulous job. There were no hitches. Thank you again for all your work planning this trip.",
+            person: "Mike taylor",
+            location: "Lahore, Pakistan"
+        },
+        {
+            text: "Jadoo was great with the entire process from planning to updates during the trip. We had 11 people and everything was perfectly executed. We appreciate all of her hard work. It was truly the trip of a lifetime. Thank you!",
+            person: "Gina",
+            location: "Owerri, Nigeria"
+        },
+        {
+            text: "Booking through you was very easy and made our lives so much easier. I have nothing bad to say! Thank you for giving us tips and guidance before we left on what to bring and such, that was very helpful!",
+            person: "Emma",
+            location: "Vancouver, Canada"
+        },
+        {
+            text: "Thank you for your recommendation and putting the trip together. The resort was absolutely beautiful. The infinity pools, the palm trees in the main pool, the infinity pool in out preferred area overlooking the gulf and the golf course were exceptional....",
+            person: "Loveth",
+            location: "Lagos, Nigeria"
+        },
+    ],
+}
+
+export const NewsletterTexts = {
+    firstText: "Subscribe to get information, latest news and other interesting offers about Jadoo.",
+    placeholderText: "Your email",
+    buttonText: "Subscribe"
+}
+
+export const FooterTexts = {
+    underLogoText: "Book your trip in minutes, get full control for much longer.",
+    quickLinks: {
+        caption: "Quick Links",
+        links: [
+            {
+                name: "Destination",
+                url: "/"
+            },
+            {
+                name: "Hotels",
+                url: "/"
+            },
+            {
+                name: "Flights",
+                url: "/"
+            },
+            {
+                name: "Bookings",
+                url: "/"
+            },
+            {
+                name: "Login",
+                url: "/"
+            },
+            {
+                name: "Signup",
+                url: "/"
+            }
+        ]
+    },
+    contacts: {
+        caption: "Contacts",
+        links: [
+            {
+                name: "FAQs",
+                url: "/"
+            },
+            {
+                name: "Help",
+                url: "/"
+            },
+            {
+                name: "Policies",
+                url: "/"
+            },
+            {
+                name: "Terms & Conditions",
+                url: "/"
+            }
+        ]
+    },
+    more: {
+        caption: "More",
+        links: [
+            {
+                name: "Career",
+                url: "/"
+            },
+            {
+                name: "Airlines",
+                url: "/"
+            },
+            {
+                name: "Airline Fees",
+                url: "/"
+            },
+            {
+                name: "Low Fare Tips",
+                url: "/"
+            }
+        ]
+    }
+}
